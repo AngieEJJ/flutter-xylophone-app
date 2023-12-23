779c4b474201505402c0d9230d84a535077f5526
eunjinjeong <eunjin.jeong.5115@gmail.com>
2023-12-23 17:09:14 +0900

    MainScreen생성 및 가로모드 고정

---
+++ lib/main_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatelessWidget {
+  const MainScreen({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('실로폰')),
+    );
+  }
+}
