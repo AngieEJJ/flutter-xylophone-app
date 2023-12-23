24011272c568b20ba2d02b30677510257a0d5789
eunjinjeong <eunjin.jeong.5115@gmail.com>
2023-12-23 17:26:53 +0900

    메인스크린 keybord widget 구현

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -3,11 +3,57 @@ import 'package:flutter/material.dart';
 class MainScreen extends StatelessWidget {
   const MainScreen({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('실로폰')),
+        appBar: AppBar(title: const Text('실로폰')),
+        body: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            keybord('도', Colors.red),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8),
+              child: keybord('레', Colors.orange),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16),
+              child: keybord('미', Colors.yellow),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 24),
+              child: keybord('파', Colors.green),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 32),
+              child: keybord('솔', Colors.blue),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 40),
+              child: keybord('라', Colors.blueGrey),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 48),
+              child: keybord('시', Colors.purple),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 56),
+              child: keybord('도', Colors.red),
+            ),
+          ],
+        ));
+  }
+
+  Widget keybord(String text, Color color) {
+    return Container(
+      width: 50,
+      height: double.infinity,
+      color: color,
+      child: Center(
+        child: Text(
+          text,
+          style: TextStyle(fontSize: 20, color: Colors.white),
+        ),
+      ),
     );
   }
 }
